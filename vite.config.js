f0b32f6a59c024b1a9c9e415ded6fd0a80d43f28
mikiayte <mikaayta@gmail.com>
2023-07-29 03:33:24 -0700

    enviroment variable updated

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://mern-task-app-api-2t9l.onrender.com',
         changeOrigin : true
       }
     }
